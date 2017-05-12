dbb62e5d4ca8af3ae41ae589f911fdd18dc6c61f
Czerny-F <dummy00.cz@gmail.com>
2017-05-12 19:48:37 +0900

    django: tutorial01 && from/import

--- src/django/mysite/polls/urls.py
+++ src/django/mysite/polls/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 
-from polls import views
-import polls
+from . import views
+
+from polls.views import *
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
-
-    url(r'^hoge/', views.index),
+    url(r'^$', views.index, name='index'),
+    url(r'^foo/', index),
+    url(r'^bar/', test_method),
 ]
--- src/django/mysite/polls/views.py
+++ src/django/mysite/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request, param='no params'):
+    print(param)
+    print(request)
+
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def test_method(request):
+    return HttpResponse("Test method called")
+    # pass
