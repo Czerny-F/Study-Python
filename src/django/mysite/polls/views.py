a0a6a97ad8725db830147e0722c09f9ad2bb443e
Czerny-F <dummy00.cz@gmail.com>
2017-09-13 17:20:13 +0900

    refactoring polls.views

--- src/django/mysite/polls/views.py
+++ src/django/mysite/polls/views.py
@@ -1,34 +1,33 @@
-from django.shortcuts import render, get_object_or_404  # noqa
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader  # noqa
 
 from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list}
-    return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
 
 
 def test_method(request):
     raise Http404("Question does not exist")
 
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
 def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
     return render(request, 'polls/detail.html', {'question': question})
 
 
