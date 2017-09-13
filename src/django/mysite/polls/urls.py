1952f7d9fd1d8f1f9f1d20a0acf0b154c8e66d96
Czerny-F <dummy00.cz@gmail.com>
2017-09-13 16:56:57 +0900

    django tutorial - view, template

--- src/django/mysite/polls/urls.py
+++ src/django/mysite/polls/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
@@ -27,4 +28,6 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    url(r'^hoge/$', views.test_method),
 ]
--- src/django/mysite/polls/views.py
+++ src/django/mysite/polls/views.py
@@ -1,23 +1,35 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import render, get_object_or_404  # noqa
+from django.http import Http404
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def test_method(request):
-    return HttpResponse("Test method called")
-    # pass
+    raise Http404("Question does not exist")
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
