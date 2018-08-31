024c2d00df6dc4c0673fc6dcaa0f825de19a28ae
Czerny-F <dummy00.cz@gmail.com>
2018-08-31 15:49:40 +0900

    flask: routing

--- flask/hello.py
+++ flask/hello.py
@@ -3,6 +3,32 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello, World'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    print(type(username))
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    print(type(post_id))
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    print(type(subpath))
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
