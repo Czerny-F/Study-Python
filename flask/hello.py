811d2155206723a8cb72d5973f61331f32b9ab78
Czerny-F <dummy00.cz@gmail.com>
2018-08-31 17:30:47 +0900

    flask: ~static

--- flask/hello.py
+++ flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,17 @@ def index():
     return 'Index Page'
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     return 'Hello, World'
+
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+    print('Request posted to hello')
 
 
 @app.route('/user/<username>')
@@ -32,3 +40,26 @@ def show_subpath(subpath):
     print(type(subpath))
     # show the subpath after /path/
     return 'Subpath %s' % subpath
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    print(request)
+    print(type(request))
+    print(request.__class__.mro())
+    print(request.method)
+    # print(vars(request))
+    return 'The about page'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('projects'))
+    print(url_for('projects', next='/'))
+    print(url_for('show_user_profile', username='John Doe'))
+    print(url_for('static', filename='style.css'))
