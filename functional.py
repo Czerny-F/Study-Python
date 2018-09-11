c8ea6d6a839d98f7dbcd52922d21bd89f9bab15b
Czerny-F <dummy00.cz@gmail.com>
2018-09-11 18:57:45 +0900

    decorator

--- functional.py
+++ functional.py
@@ -1,5 +1,55 @@
+from inspect import currentframe
+
+
 def curry(func, *args, **kwargs):
     def _curried(*_args, **_kwargs):
         return func(*args, *_args, **{**kwargs, **_kwargs})
 
     return _curried
+
+
+def curring(*args, **kwargs):
+    def deco(func):
+        def curried(*_args, **_kwargs):
+            return func(*args, *_args, **{**kwargs, **_kwargs})
+        return curried
+    return deco
+
+
+def noargs_deco(func):
+    print('scope:', currentframe().f_code.co_name)
+
+    def wrapper(*args, **kwargs):
+        print('scope:', currentframe().f_code.co_name)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def noret_deco(*dargs, **dkwargs):
+    print('scope:', currentframe().f_code.co_name)
+    print('args:', dargs, dkwargs)
+
+    def wrapper(func):
+        print('scope:', currentframe().f_code.co_name)
+        print('func:', func)
+        return func
+
+    return wrapper
+
+
+def full_deco(*dargs, **dkwargs):
+    print('scope:', currentframe().f_code.co_name)
+    print('args:', dargs, dkwargs)
+
+    def deco(func):
+        print('scope:', currentframe().f_code.co_name)
+        print('func:', func)
+
+        def wrapper(*args, **kwargs):
+            print('scope:', currentframe().f_code.co_name)
+            print('args:', args, kwargs)
+
+            return func(*args, **kwargs)
+        return wrapper
+    return deco
